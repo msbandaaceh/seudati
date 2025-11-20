b8278ef5252c68e6f0a84b12614a5c406b12a1e1
Awan Widodo <kurniawan.adi.widodo@gmail.com>
2025-11-20 15:44:56 +0700

    update : pembaruan routes dan layout seudati versi 1.4.0

--- application/config/routes.php
+++ application/config/routes.php
@@ -89,6 +89,7 @@ $route['cari_cuti'] = 'HalamanLaporan/cari_cuti';
 $route['show_pegawai'] = 'HalamanUtama/show_pegawai';
 $route['show_cuti_admin'] = 'HalamanCuti/show_cuti_admin';
 $route['simpan_cuti_admin'] = 'HalamanCuti/simpan_cuti_admin';
+$route['get_pegawai_id_from_cuti'] = 'HalamanCuti/get_pegawai_id_from_cuti';
 
 # ROUTE PROSES PERAN
 $route['show_role'] = 'HalamanUtama/show_role';
